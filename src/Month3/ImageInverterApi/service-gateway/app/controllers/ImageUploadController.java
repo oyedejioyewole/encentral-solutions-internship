8f9b719ea2cf51607806323f8b0c3f00b80625a0
oyedejioyewole <57059138+oyedejioyewole@users.noreply.github.com>
2025-10-27 16:31:55 +0100

    refactor:🔄 Remove unused import in ImageUploadController.

--- src/Month3/ImageInverterApi/service-gateway/app/controllers/ImageUploadController.java
+++ src/Month3/ImageInverterApi/service-gateway/app/controllers/ImageUploadController.java
@@ -4,7 +4,6 @@ import com.encentral.image_inverter.impl.ImageProcessingService;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import play.libs.Json;
-import play.mvc.BodyParser;
 import play.mvc.Controller;
 import play.mvc.Http;
 import play.mvc.Result;
